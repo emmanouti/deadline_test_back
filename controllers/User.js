fb1e5d786ff09d3ca6ea91d3e3304260b1c202f5
Emma <emma.nouti@gmail.com>
2022-10-10 13:42:20 +0200

    feat/controllers : user simple route controller

---
+++ controllers/User.js
@@ -0,0 +1,7 @@
+const getUsers = (req, res) => {
+    res.send("user route");
+};
+
+module. exports = {
+    getUsers,
+}
--- router.js
+++ router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-
+const {getUsers} = require("./controllers/User")
 router.get("/", (req, res) => {
     res.send("Router ok");
 });
+router.get("/users", getUsers);
 
 module.exports = router;
